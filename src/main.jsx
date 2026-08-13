f89c14962d083b36fb75e356f843c94575a6b63e
luciecreations60 <luciecreations60@gmail.com>
2026-08-13 12:26:43 +0200

    fix(providers): wrap app with ThemeProvider and TripProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './auth/AuthContext.jsx';
 import { LocalizationProvider } from './contexts/LocalizationContext.jsx';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { TripProvider } from './contexts/TripContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LocalizationProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <TripProvider>
+            <App />
+          </TripProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </LocalizationProvider>
   </React.StrictMode>
 );
