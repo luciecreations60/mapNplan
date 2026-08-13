32d8cb279296429e30f73d210ce323f69babbc69
luciecreations60 <luciecreations60@gmail.com>
2026-08-13 12:35:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/main.css';
 import App from './App.jsx';
 import { AuthProvider } from './auth/AuthContext.jsx';
 import { LocalizationProvider } from './contexts/LocalizationContext.jsx';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
 import { TripProvider } from './contexts/TripContext.jsx';
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,5 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </AuthProvider>
       </ThemeProvider>
     </LocalizationProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
